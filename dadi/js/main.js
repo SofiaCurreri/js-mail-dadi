282e8478bdc01b6edd69cf8866ccbab4ad69cf0f
SofiaCurreri <sofiacurreri@gmail.com>
2023-01-27 10:04:28 +0100

    aggiunto event listener click

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,17 +1,23 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-const randomUser = parseInt((Math.random() * 6) + 1);
-const randomComputer = parseInt((Math.random() * 6) + 1);
+const inputButton = document.getElementById("play");
 
-document.getElementById("num-user").innerHTML = "Il numero random del giocatore è " + randomUser;
-document.getElementById("num-computer").innerHTML = "Il numero random del computer è " + randomComputer;
+inputButton.addEventListener(
+    "click",
+    function () {
+        const randomUser = parseInt((Math.random() * 6) + 1);
+        const randomComputer = parseInt((Math.random() * 6) + 1);
 
+        document.getElementById("num-user").innerHTML = "Il numero random del giocatore è " + randomUser;
+        document.getElementById("num-computer").innerHTML = "Il numero random del computer è " + randomComputer;
 
-if (randomUser == randomComputer) {
-    document.getElementById("vincitore").innerHTML = "Pareggio!";
-} else if (randomUser > randomComputer) {
-    document.getElementById("vincitore").innerHTML = "Il vincitore è il giocatore";
-} else {
-    document.getElementById("vincitore").innerHTML = "Il vincitore è il computer";
-}
+        if (randomUser == randomComputer) {
+            document.getElementById("vincitore").innerHTML = "Pareggio!";
+        } else if (randomUser > randomComputer) {
+            document.getElementById("vincitore").innerHTML = "Il vincitore è il giocatore";
+        } else {
+            document.getElementById("vincitore").innerHTML = "Il vincitore è il computer";
+        }
+    }
+);
